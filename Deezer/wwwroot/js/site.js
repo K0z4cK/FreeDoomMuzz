ba607a2e9fd6802c5d18e001d822ce46d9423808
user <user@A25PC11>
2020-02-18 19:53:23 +0400

    some design changes

--- Deezer/wwwroot/js/site.js
+++ Deezer/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function validateForm() {
+    let userName = document.forms["myForm"]["UserName"].value;
+    let firstName = document.forms["myForm"]["fEmail"].value;
+    let lastName = document.forms["myForm"]["fEmail"].value;
+    let password = document.forms["myForm"]["fEmail"].value;
+    let passwordConfirm = document.forms["myForm"]["fEmail"].value;
+    if (userName.Length >= 5) {
+        document.["UserName"].value = "User First Name length must be more than 4 symbols";
+        return false;
+    }
+}
